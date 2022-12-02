09e67b3fefcdd96bf40f885017b1620e6dbc6930
Sergio Garcia <segarciat314@gmail.com>
2022-12-02 15:58:37 -0500

    Loof and timmy for card project

---
+++ js/card-project.js
@@ -0,0 +1,31 @@
+{
+  /* <div class="col-lg-3 col-md-4 col-sm-6">
+  <div class="card">
+    <img src="https://via.placeholder.com/150" class="card-img-top" alt="..." />
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">
+        Some quick example text to build on the card title and make up the bulk
+        of the card's content.
+      </p>
+
+      <button
+        type="button"
+        data-bs-toggle="modal"
+        data-bs-target="#addModal"
+        class="btn btn-warning"
+      >
+        Update
+      </button>
+      <button
+        data-bs-toggle="modal"
+        data-bs-target="#deleteModal"
+        type="button"
+        class="btn btn-danger"
+      >
+        Delete
+      </button>
+    </div>
+  </div>
+</div>; */
+}
