95e8a7ae9e2089cf931de1467b045c19a91027ec
Sergio Garcia <segarciat314@gmail.com>
2022-12-05 16:29:12 -0500

    Populate UI on load and handle card deletion

--- card-project/js/index.js
+++ card-project/js/index.js
@@ -1,9 +1,15 @@
 // Constants
 const CARD_PROJECT_DATA_KEY = "card-project-data";
+const CARD_TITLE_ATTRIBUTE = "title-of-card-to-delete"; // to know which card to delete.
 const addForm = document.getElementById("addForm");
+const deleteModalForm = document.querySelector("#deleteModal form");
+const cardContainer = document.getElementById("cardContainer");
 
 // Set up event listeners.
 addForm.addEventListener("submit", handleAddFormSubmit);
+deleteModalForm.addEventListener("submit", handleConfirmDelete);
+
+window.addEventListener("load", populateUI);
 
 // When add form is submitted, it updates the UI with the given data, and saves that data.
 function handleAddFormSubmit(e) {
@@ -14,7 +20,7 @@ function handleAddFormSubmit(e) {
     description: addForm.description.value,
     imageUrl: addForm.imageUrl.value,
   };
-  addCardToUI(cardData);
+  addNewCardToUI(cardData);
   saveCardData(cardData);
 
   // Close the modal.
@@ -28,10 +34,17 @@ function saveCardData(cardData) {
   localStorage.setItem(CARD_PROJECT_DATA_KEY, JSON.stringify(data));
 }
 
+// Load from data from localStorage and display in UI.
+function populateUI() {
+  const data = JSON.parse(localStorage.getItem(CARD_PROJECT_DATA_KEY)) || [];
+  data.forEach((cardData) => addNewCardToUI(cardData));
+}
+
 // Decide on max length for title and for description.
-function addCardToUI(cardData) {
+function addNewCardToUI(cardData) {
   const cardCol = document.createElement("div");
   cardCol.classList = "col-lg-3 col-md-4 col-sm-6";
+  const deleteBtnKeyval = 'data-action="delete"'; // used to target delete button.
   cardCol.innerHTML = `
   <div class="card">
     <img src="${cardData.imageUrl}" class="card-img-top" alt="..." />
@@ -51,6 +64,7 @@ function addCardToUI(cardData) {
       </button>
       <button
         data-bs-toggle="modal"
+        ${deleteBtnKeyval}
         data-bs-target="#deleteModal"
         type="button"
         class="btn btn-danger"
@@ -60,6 +74,39 @@ function addCardToUI(cardData) {
     </div>
   </div>
   `;
-  const cardContainer = document.getElementById("cardContainer");
   cardContainer.append(cardCol);
+  const deleteBtn = cardCol.querySelector(`[${deleteBtnKeyval}]`);
+  deleteBtn.setAttribute(CARD_TITLE_ATTRIBUTE, cardData.title);
+  deleteBtn.addEventListener("click", handleDeleteClick);
+}
+
+// Save title of the card to be deleted on the modal.
+function handleDeleteClick(e) {
+  const title = e.target.getAttribute(CARD_TITLE_ATTRIBUTE);
+  deleteModalForm.setAttribute(CARD_TITLE_ATTRIBUTE, title);
+}
+
+function handleConfirmDelete(e) {
+  e.preventDefault();
+  const title = deleteModalForm.getAttribute(CARD_TITLE_ATTRIBUTE);
+  deleteCardFromDB(title);
+  deleteModalForm.removeAttribute(CARD_TITLE_ATTRIBUTE);
+  deleteCardFromUI(title);
+  document.getElementById("cancelModalBtn").click(); // hide modal.
+}
+
+function deleteCardFromDB(title) {
+  // Load from LS.
+  let data = JSON.parse(localStorage.getItem(CARD_PROJECT_DATA_KEY)) || [];
+
+  // Remove the element.
+  data = data.filter((cardData) => cardData.title !== title);
+
+  // Save to local storage.
+  localStorage.setItem(CARD_PROJECT_DATA_KEY, JSON.stringify(data));
+}
+
+function deleteCardFromUI(title) {
+  const btn = document.querySelector(`[${CARD_TITLE_ATTRIBUTE}="${title}"]`);
+  btn.closest(".card").parentElement.remove(); // remove col which is parent of card.
 }
