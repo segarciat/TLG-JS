076e8228024d1035540ff0bda465ba4b8ace81b1
Sergio Garcia <segarciat314@gmail.com>
2022-12-08 12:22:31 -0500

    Refactored

--- memory-game/js/index.js
+++ memory-game/js/index.js
@@ -5,7 +5,6 @@
  * Global constants.
  */
 const BACK_CARD_SRC = "./img/back-card.svg";
-const MAX_CARDS = 16;
 const UNIQUE_IMAGES_SRC = [
   "https://i.giphy.com/media/bMSaGMYM8OvsN2gKb6/giphy.webp",
   "https://i.giphy.com/media/t3cL1iKETUqlBaQ6YV/giphy.webp",
@@ -16,7 +15,9 @@ const UNIQUE_IMAGES_SRC = [
   "https://i.giphy.com/media/jOywF6yzPBJ2D9KK1x/giphy.webp",
   "https://i.giphy.com/media/VbtUysLNF86UaOwxKm/giphy.webp",
 ];
-const FLIP_ANIMATION_DURATION = 2000;
+const MAX_CARDS = 16;
+const UNFLIP_DELAY = 2000;
+const FLIP_CARD_CLS = "flip-card";
 
 // Possible states card can be in.
 const REVEALED = "revealed";
@@ -32,7 +33,7 @@ let mismatchTimeoutId; // Delayed unflip timeout ID upon mismatch.
 function addAllCardsToUI() {
   // Picked half of MAX_CARDS src strings.
   const srcs = UNIQUE_IMAGES_SRC.slice(0, MAX_CARDS / 2);
-  const shuffledSrcs = shuffled(srcs.concat(srcs));
+  const shuffledSrcs = shuffle(srcs.concat(srcs));
   shuffledSrcs.forEach((src) => addCardToUI(src));
 }
 
@@ -61,39 +62,31 @@ function addCardToUI(src) {
   `;
 
   // By default, flip cards are not revealed.
-  const card = col.querySelector(".flip-card");
+  const card = col.querySelector(`.${FLIP_CARD_CLS}`);
   card.state = HIDDEN;
   card.addEventListener("click", handleCardClick);
   document.getElementById("cardContainer").append(col);
 }
 
 function handleCardClick(e) {
-  const card = e.target.closest(".flip-card");
-  if (card.state === REVEALED) {
-    return; // ignore the click.
-  } else if (mismatchTimeoutId) {
+  const card = e.target.closest(`.${FLIP_CARD_CLS}`);
+
+  // Immediately hide the mismatched pair.
+  if (mismatchTimeoutId) {
     clearTimeout(mismatchTimeoutId);
-    const otherCard = Array.from(document.querySelectorAll(".flip-card")).find(
-      (card) => card.state === REVEALED && card != currentCard
-    );
-    handleMismatch(otherCard);
+    const pair = Array.from(document.querySelectorAll(`.${FLIP_CARD_CLS}`));
+    const card = pair.find((c) => c.state === REVEALED && c != currentCard);
+    processMismatch(card);
   }
-  // Reveal the card
-  flip(card);
-  // Check if a card has already been revealed.
-  if (!currentCard) {
-    currentCard = card;
-  } else if (checkMatch(card, currentCard)) {
-    handleMatch(card);
-  } else {
-    // Set to flip after a short time
-    mismatchTimeoutId = setTimeout(
-      () => handleMismatch(card),
-      FLIP_ANIMATION_DURATION
-    );
+
+  if (card.state === HIDDEN) {
+    handleHiddenCardClick(card);
   }
 }
 
+/**
+ * Reavel card if hidden, or hide if revealed.
+ */
 function flip(card) {
   const inner = card.querySelector(".flip-card-inner");
   const isRevealed = card.state === REVEALED;
@@ -101,16 +94,18 @@ function flip(card) {
   card.state = isRevealed ? HIDDEN : REVEALED;
 }
 
+/**
+ * See if cards have matching URLs.
+ */
 function checkMatch(cardOne, cardTwo) {
-  let imgLinkOne = cardOne
-    .querySelector(".flip-card-back img")
-    .getAttribute("src");
-  let imgLinkTwo = cardTwo
-    .querySelector(".flip-card-back img")
-    .getAttribute("src");
-  return imgLinkOne === imgLinkTwo;
+  let src1 = cardOne.querySelector(".flip-card-back img").getAttribute("src");
+  let src2 = cardTwo.querySelector(".flip-card-back img").getAttribute("src");
+  return src1 === src2;
 }
 
+/**
+ * Set card state to MATCHED, disallow further clicks, unset currentCard.
+ */
 function handleMatch(card) {
   card.removeEventListener("click", handleCardClick);
   currentCard.removeEventListener("click", handleCardClick);
@@ -119,15 +114,30 @@ function handleMatch(card) {
   currentCard = null;
 }
 
-function handleMismatch(card) {
+/**
+ * Unflip mismatched cards, unset currentCard and timeout ID.
+ */
+function processMismatch(card) {
   flip(card);
   flip(currentCard);
   mismatchTimeoutId = null;
   currentCard = null;
 }
+
+function handleHiddenCardClick(card) {
+  flip(card); // Reveal card.
+  if (!currentCard) {
+    currentCard = card;
+  } else if (checkMatch(card, currentCard)) {
+    handleMatch(card);
+  } else {
+    mismatchTimeoutId = setTimeout(() => processMismatch(card), UNFLIP_DELAY);
+  }
+}
+
 // https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle#The_modern_algorithm
 /* Randomize array in-place using Durstenfeld shuffle algorithm */
-function shuffled(array) {
+function shuffle(array) {
   // Copy the array (don't modify original).
   const shuffled = array.slice(0);
 
