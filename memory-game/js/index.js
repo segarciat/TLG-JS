9f87575a3b4d7f9947c339042d4d57d4962afa68
Sergio Garcia <segarciat314@gmail.com>
2022-12-08 09:24:44 -0500

    Added Fixed set of image SRCs, method to shuffle array and add cards to UI

--- memory-game/js/index.js
+++ memory-game/js/index.js
@@ -1,5 +1,40 @@
+"use strict";
 // https://upload.wikimedia.org/wikipedia/commons/d/d8/Atlas_deck_card_back_blue_and_brown.svg
 const BACK_CARD_SRC = "./img/back-card.svg";
+const MAX_CARDS = 16;
+const UNIQUE_IMAGES_SRC = [
+  "https://i.giphy.com/media/bMSaGMYM8OvsN2gKb6/giphy.webp",
+  "https://i.giphy.com/media/t3cL1iKETUqlBaQ6YV/giphy.webp",
+  "https://i.giphy.com/media/cnWLVExrejEiZz5xa9/giphy.webp",
+  "https://i.giphy.com/media/jqIhAXf3SBpMxVpYsB/giphy.webp",
+  "https://i.giphy.com/media/ZSJCdxWnrXuJG6vDbB/giphy.webp",
+  "https://i.giphy.com/media/XVC9ElNTFmZ8yxyBOW/giphy.webp",
+  "https://i.giphy.com/media/jOywF6yzPBJ2D9KK1x/giphy.webp",
+  "https://i.giphy.com/media/VbtUysLNF86UaOwxKm/giphy.webp",
+];
+
+function addAllCardsToUI() {
+  // Picked half of MAX_CARDS src strings.
+  const srcs = UNIQUE_IMAGES_SRC.slice(0, MAX_CARDS / 2);
+  const shuffledSrcs = shuffled(srcs.concat(srcs));
+  shuffledSrcs.forEach((src) => addCardToUI(src));
+}
+
+// https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle#The_modern_algorithm
+/* Randomize array in-place using Durstenfeld shuffle algorithm */
+function shuffled(array) {
+  // Copy the array (don't modify original).
+  const shuffled = array.slice(0);
+
+  // Shuffle it.
+  for (var i = shuffled.length - 1; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1));
+    var temp = shuffled[i];
+    shuffled[i] = shuffled[j];
+    shuffled[j] = temp;
+  }
+  return shuffled;
+}
 
 // let flipCards = document.querySelectorAll(".flip-card");
 // let currentCard;
@@ -51,7 +86,7 @@ function unflipCards(cardOne, cardTwo) {
   currentCard = null;
 }
 
-function addCardToUI() {
+function addCardToUI(src) {
   const col = document.createElement("div");
   col.classList = "col-md-3";
   col.innerHTML = `
@@ -66,8 +101,8 @@ function addCardToUI() {
       </div>
       <div class="flip-card-back">
         <img
-          src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/cute-baby-animals-1558535060.jpg"
-          alt="Avatar"
+          src="${src}"
+          alt="Matching-card"
           style="width: 300px; height: 300px"
         />
       </div>
@@ -75,7 +110,9 @@ function addCardToUI() {
   </div>
   `;
 
+  // By default, flip cards are not revealed.
+  col.document.querySelector(".flip-card").revealed = false;
   document.getElementById("cardContainer").append(col);
 }
 
-addCardToUI();
+addAllCardsToUI();
