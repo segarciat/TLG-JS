fa3be79ed04459cdec28477898d8f42da41f6eaf
Sergio Garcia <segarciat314@gmail.com>
2022-12-08 17:57:39 -0500

    Form submission behavior enabled

--- loan-calculator/js/index.js
+++ loan-calculator/js/index.js
@@ -8,20 +8,43 @@ const resultArea = document.getElementById("monthlyPayments");
 
 /** Retrieve form values. Returns object: {amount, years, rate}. */
 
-function getFormValues() {}
+function getFormValues() {
+  return {
+    amount: amountInput.value,
+    years: yearsInput.value,
+    rate: rateInput.value,
+  };
+}
 
 /** Calculate monthly payment and return. */
 
-function calcMonthlyPayment(amount, years, rate) {}
+function calcMonthlyPayment(amount, years, rate) {
+  return 0;
+}
 
 /** Get form values, calculate, format to 2 decimal places, and display. */
 
-function getFormValuesAndDisplayResults() {}
+function getFormValuesAndDisplayResults(e) {
+  e.preventDefault();
+  const values = getFormValues();
+  const monthlyPayments = calcMonthlyPayment(values).toFixed(2);
+  resultArea.textContent = `$${monthlyPayments}`;
+}
 
 /** Set initial form values and show initial results. Called at app start. */
 
-function setInitialValues() {}
+function setInitialValues() {
+  amountInput.value = 10000;
+  yearsInput.value = 10;
+  rateInput.value = 4.5;
+}
 
 /** Start: set form defaults & display; attach form submit event listener. */
 
-function start() {}
+function start() {
+  calcForm.addEventListener("submit", getFormValuesAndDisplayResults);
+  setInitialValues();
+  calcForm.requestSubmit();
+}
+
+start();
