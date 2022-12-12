1714f21fd9ee85b1e6620ec118393f6afbce4144
Sergio Garcia <segarciat314@gmail.com>
2022-12-12 14:56:05 -0500

    Created simple navbar with application's name

--- react-dictionary/src/App.js
+++ react-dictionary/src/App.js
@@ -1,5 +1,12 @@
+import Navbar from "./Navbar";
+
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <div className="App">
+      <Navbar />
+      <h1>Hello</h1>
+    </div>
+  );
 }
 
 export default App;
---
+++ react-dictionary/src/Navbar.js
@@ -0,0 +1,12 @@
+import "./index.css";
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="nav-content">
+        <h2>React Dictionary</h2>{" "}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
