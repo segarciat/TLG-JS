af8cc6bd3ae18d7aecbe95cce6015beb70bda8c0
Sergio Garcia <segarciat314@gmail.com>
2022-12-12 15:20:22 -0500

    Make request to Merriam Webster API for word definition

--- react-dictionary/src/App.js
+++ react-dictionary/src/App.js
@@ -2,14 +2,27 @@ import Navbar from "./Navbar";
 import SearchForm from "./SearchForm";
 import { useState } from "react";
 
+const API_URL = "https://www.dictionaryapi.com/api/v3/references/sd4/json";
+const API_KEY = process.env.REACT_APP_API_KEY;
+
 function App() {
-  const [word, setWord] = useState("");
+  const [word, setWord] = useState(null);
 
   function handleSearchSubmit(e) {
     e.preventDefault();
     // Get the word the user searched for.
     const searchInputValue = e.target.elements.searchWordInput.value;
 
+    // Make API request with the word.
+    fetch(`${API_URL}/${searchInputValue}?key=${API_KEY}`)
+      .then((res) => res.json())
+      .then((data) => {
+        const result = data.find((w) => w.meta.id === searchInputValue);
+        setWord({
+          searchTerm: searchInputValue,
+          definition: result.shortdef[0],
+        });
+      });
     // Display word in UI for now.
     setWord(searchInputValue);
     e.target.reset(); // reset the form.
@@ -19,8 +32,8 @@ function App() {
       <Navbar />
       <div className="container">
         <SearchForm handleSubmit={handleSearchSubmit} />
-        <h1>Word: {word}</h1>
-        <p>Definition: </p>
+        <h1>Word: {word && word.searchTerm}</h1>
+        <p>Definition: {word && word.definition}</p>
       </div>
     </div>
   );
